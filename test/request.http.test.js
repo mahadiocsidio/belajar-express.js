3e3efb7a2851cda741f8deec911c1034e6bcc2b3
mahadiocsidio <mahadiocsidio@students.amikom.ac.id>
2023-06-20 00:09:01 +0700

    unit test 2

--- test/request.http.test.js
+++ test/request.http.test.js
@@ -4,11 +4,11 @@ import request from "supertest"
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.send(`Hello ${req.query.name}`);
 });
 
-test("Test ExpressJS", async()=>{
-    const response = await request(app).get("/");
+test("Test Querry", async()=>{
+    const response = await request(app).get("/").query({name: "World"});
     expect(response.text).toBe("Hello World");
 });
 
